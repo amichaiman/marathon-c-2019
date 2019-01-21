d291eba13b01512cc6fdf947ae69f21b448d137a
amichaiman <ami.dan.man@gmail.com>
2019-01-21 10:17:08 +0200

    first commit

--- 1.c
+++ 1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int num_nums(int a[], int size, int n) {
-    if (size <= 0) {
+
+int num_sums(int *a, int size, int n) {
+    if (size<=0) {
         return 0;
     }
-    if (a[0] > n) {
-        return num_nums(a+1, size-1, n);
-    } else if (a[0] < n) {
-        return num_nums(a+1, size-1, n-a[0]) + num_nums(a+1, size-1, n);
+    if (*a > n) {
+        return num_sums(a+1, size-1, n);
     }
-    return 1;
+    return *a == n ? 1+num_sums(a+1, size-1, n)
+                   : num_sums(a+1, size-1, n-*a) + num_sums(a+1, size-1, n);
 }
 
 int main() {
     int a[] = {3,1,4,5,3,7,8,2};
-    printf("%d\n", num_nums(a, 8, 7));
+    printf("%d", num_sums(a, sizeof(a)/sizeof(*a), 7));
     return 0;
 }
