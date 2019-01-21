162852c1b124e7dd1f7ad98fe452b4dc04d18805
amichaiman <ami.dan.man@gmail.com>
2019-01-21 11:40:53 +0200

    first commit

--- 9.c
+++ 9.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void sort_strings(char **strings, int number_of_strings) {
+
+void arrange_arguments(int size, char **strings);
+
+void print_arguments(int size, char **strings);
+
+int main(int argc, char **argv) {
+    arrange_arguments(argc, argv);
+    print_arguments(argc, argv);
+    return 0;
+}
+
+void print_arguments(int size, char **strings) {
+    int i;
+    for (i=1; i<size; i++) {
+        puts(strings[i]);
+    }
+}
+
+void arrange_arguments(int size, char **strings) {
     int i,j;
-    for (i=number_of_strings; i>1; i--) {
+    for (i=size; i>1; i--) {
         for (j=1; j<i-1; j++) {
-            if (strlen(strings[j]) > strlen(strings[j+1]) || strlen(strings[j]) == strlen(strings[j+1])) {
-                char *temp = strings[j];
+            if (strlen(strings[j]) < strlen(strings[j+1]) ||        //if j is shorter j+1
+                    (strlen(strings[j]) == strlen(strings[j+1]) &&  //if j == j+1
+                    strcmp(strings[j], strings[j+1]) > 0)) {        //and j > j+1 in dictionary
+                /* swap */
+                char* temp = strings[j];
                 strings[j] = strings[j+1];
                 strings[j+1] = temp;
             }
         }
     }
-}
-
-int main(int argc, char **argv) {
-    int i;
-    sort_strings(argv, argc);
-    for (i=0; i<argc; i++) {
-        puts(argv[i]);
-    }
-    return 0;
 }
 
