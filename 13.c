7f172f5d74097e30421d9871b625257d06ee6f1a
amichaiman <ami.dan.man@gmail.com>
2019-01-22 11:32:44 +0200

    first commit

--- 13.c
+++ 13.c
@@ -1,87 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define BOARD_SIZE 8
 
 typedef enum {white, black} color;
 
-
 typedef struct {
+    color c;
     int i;
     int j;
-    color c;
-} Peice;
+} Piece;
 
-int in_board(int i, int j) {
-    return i>=0 && j>=0 && j<BOARD_SIZE && i<BOARD_SIZE;
-}
-
-int valid_move(Peice* p, int to_i, int to_j) {
-    if (p->c == white && p->i-to_i == 1 && abs(p->j-to_j) == 1) {
-        return 1;
-    }
-    if (p->c == black && to_i-p->i == 1 && abs(to_j-p->j) == 1) {
-        return 1;
-    }
-    return 0;
-
-}
-
-int move_piece(Peice* board[][BOARD_SIZE], Peice* to_move, int to_i, int to_j) {
-    if (!in_board(to_i, to_j) || board[to_i][to_j] || !valid_move(to_move, to_i, to_j)) {
-        return 0;
-    }
-    return 1;
-}
-
-void print_board(Peice* board[][BOARD_SIZE]) {
-    int i,j;
-
+void print_board(Piece* board[][BOARD_SIZE]) {
+    int i, j;
     for (i=0; i<BOARD_SIZE; i++) {
-        for (j = 0; j < BOARD_SIZE; j++) {
-            printf("%c\t", board[i][j] == NULL ? '-' : (board[i][j]->c == black ? 'b' : 'w'));
+        for (j=0; j<BOARD_SIZE; j++) {
+            printf("%c\t", board[i][j] ? (board[i][j]->c == black ? 'b' : 'w' ): '-');
         }
         putchar('\n');
     }
 }
 
-void start_board(Peice* board[][BOARD_SIZE]) {
-    int i,j;
+int is_even(int n) {
+    return !(n%2);
+}
 
+void start_board(Piece* board[][BOARD_SIZE]) {
+    int i,j;
+    /* initialize black pieces */
     for (i=0; i<3; i++) {
         for (j=0; j<BOARD_SIZE; j++) {
-            if ((i+j)%2 == 0){
+            if (is_even(i+j)) {
                 board[i][j] = NULL;
             } else {
-                board[i][j] = (Peice*) malloc (sizeof(Peice));
+                board[i][j] = (Piece*) malloc(sizeof(Piece));
+                if (!board[i][j]) {
+                    exit(1);
+                }
                 board[i][j]->c = black;
+                board[i][j]->i = i;
+                board[i][j]->j = j;
             }
         }
     }
 
+    /* initialize empty lines */
     for (i=3; i<5; i++) {
         for (j = 0; j < BOARD_SIZE; j++) {
             board[i][j] = NULL;
         }
     }
 
-    for (i=5; i<8; i++) {
+    /* initialize white pieces */
+    for (i=5; i<BOARD_SIZE; i++) {
         for (j=0; j<BOARD_SIZE; j++) {
-            if ((i+j)%2 == 0){
+            if (is_even(i+j)) {
                 board[i][j] = NULL;
             } else {
-                board[i][j] = (Peice*) malloc (sizeof(Peice));
+                board[i][j] = (Piece*) malloc(sizeof(Piece));
+                if (!board[i][j]) {
+                    exit(1);
+                }
                 board[i][j]->c = white;
+                board[i][j]->i = i;
+                board[i][j]->j = j;
             }
         }
     }
+}
+
+int in_board(int i, int j) {
+    return i>=0 && j>=0 && i<BOARD_SIZE && j<BOARD_SIZE;
+}
+
+int valid_move(Piece* piece, int to_i, int to_j) {
+    if (!in_board(piece->i, piece->j)) {
+        return 0;
+    }
+    if (to_j+1 != piece->j && to_j-1 != piece->j) {
+        return 0;
+    }
+    if (piece->c == white && piece->i != to_i+1) {
+        return 0;
+    }
+    if (piece->c == black && piece->i != to_i-1) {
+        return 0;
+    }
+    return 1;
+}
+
+int move_piece(Piece* board[][BOARD_SIZE], Piece* piece, int to_i, int to_j) {
+    //     index out of board        not empty         not valid in compared to piece
+    if (!in_board(to_i, to_j) || board[to_i][to_j] || !valid_move(piece, to_i, to_j)) {
+        return 0;
+    }
+}
+
+
+int main() {
+    Piece* board[BOARD_SIZE][BOARD_SIZE];
+
+    start_board(board);
+
     print_board(board);
 }
 
-int main() {
-    Peice* board[BOARD_SIZE][BOARD_SIZE];
-    start_board(board);
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+
 
 
